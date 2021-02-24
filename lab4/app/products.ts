1d8a662e9be1bae412dc10b0981a6864a4db36b3
aibike28 <64321776+aibike28@users.noreply.github.com>
2021-02-24 21:11:38 +0600

    Add files via upload

---
+++ lab4/app/products.ts
@@ -0,0 +1,103 @@
+export const products = [
+  {
+    id: 1,
+    name: 'DECORATIVE GLASS BOTTLE',
+    price: 49.99,
+    description: 'Decorative glass bottle with frosted effect. manufacture of recycled glass has a lower environmental impact; it consumes less energy and uses no natural raw materials.',
+    rating: '5/5',
+    source: 'https://m.media-amazon.com/images/I/71dtn2ZMs7L._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Marvels-Spider-Man-Miles-Morales-Launch-PlayStation/dp/B08JHVG9ZJ/ref=sr_1_7?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105965&sr=8-7'
+  },
+  {
+    id: 2,
+    name: 'GREEN SLATE BOWL',
+    price: 59.99,
+    description: 'Green slate bowl. As this is a natural material, the finish may vary from one piece to another.',
+    rating: '4.5/5',
+    source: 'https://m.media-amazon.com/images/I/81aEa8zePHL._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Persona-5-Strikers-Nintendo-Switch/dp/B08Q6QMND4/ref=sr_1_27_sspa?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105198&sr=8-27-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFMNzJJSzE0TzlGVzMmZW5jcnlwdGVkSWQ9QTAzMzYyMjU4MFlWV1ZTM0JMNVcmZW5jcnlwdGVkQWRJZD1BMDQ3MjI5NzI2QlVQRzRWUzdQOEQmd2lkZ2V0TmFtZT1zcF9tdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
+  },
+  {
+    id: 3,
+    name: 'TERRACOTTA PLATE',
+    price: 55.00,
+    description: 'Decorative crackled white terracotta plate.',
+    rating: '5/5',
+    source: 'https://m.media-amazon.com/images/I/81Bzm37TIaL._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Ghost-Tsushima-PlayStation-4/dp/B08BSKT43L/ref=sr_1_13?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105965&sr=8-13'
+  },
+  {
+    id: 4,
+    name: 'IRREGULAR WOODEN TRAY',
+    price: 59.99,
+    description: 'Wooden tray with an irregular base in rustic colours. Perfect as a decorative piece for any room in the house.',
+    rating: '3.7/5',
+    source: 'https://m.media-amazon.com/images/I/81iR0aGNJ5L._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Cyberpunk-2077-PlayStation-4/dp/B07DJWBYKP/ref=sr_1_16?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105965&sr=8-16'
+  },
+  {
+    id: 5,
+    name: 'CORK BLOCK WITH BAG',
+    price: 39.99,
+    description: 'Cork block providing additional support for yoga practise, helping to achieve greater stability and increase flexibility.\n' +
+      '\n' +
+      'Wooden tray with an irregular base in rustic colours. Perfect as a decorative piece for any room in the house.\n' ,
+      
+    rating: '4.7/5',
+    source: 'https://m.media-amazon.com/images/I/819zQmqyDcL._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Assassins-Valhalla-PlayStation-Standard-upgrade-4/dp/B087XTT5GZ/ref=sr_1_18?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105965&sr=8-18'
+  },
+  {
+    id: 6,
+    name: 'WOODEN FRAME MIRROR',
+    price: 59.99,
+    description: 'Round wall mirror with a wooden frame.\n' +
+      '\n',
+    rating: '2/5',
+    source: 'https://m.media-amazon.com/images/I/71IbXCVb4pL._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Mass-Effect-Legendary-PlayStation-4/dp/B08MXTDGN6/ref=sr_1_5?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105965&sr=8-5'
+  },
+  {
+    id: 7,
+    name: 'Game Therapy',
+    price: 2.99,
+    description: 'Porcelain breakfast set with an irregular design.\n',
+    rating: '3/5',
+    source: 'https://m.media-amazon.com/images/I/91cprIbMYQL._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Game-Therapy-Lorenzo-Ostuni/dp/B07FXCZYPK/ref=sr_1_29?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105198&sr=8-29'
+  },
+  {
+    id: 8,
+    name: 'The Sims 4 - Paranormal Stuff [Online Game Code]',
+    price: 9.99,
+    description: 'Light the candles and prepare the rituals to appease your ghastly new roommates in The Sims 4 Paranormal Stuff Pack*! Spectral guests cause mischief and mayhem on Haunted House Lots, but Sims can perform a séance, befriend Guidry the Ghost, become a Paranormal Investigator, and restore order. *REQUIRES THE SIMS 4 (SOLD SEPARATELY) AND ALL GAME UPDATES.\n',
+    rating: '4.6/5',
+    source: 'https://m.media-amazon.com/images/I/81VOyqGZ44L._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Sims-Paranormal-Stuff-Online-Game/dp/B08V1JDVW5/ref=sr_1_30?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105198&sr=8-30'
+  },
+  {
+    id: 9,
+    name: 'Resident Evil Village Deluxe Edition - PlayStation 5 Deluxe Edition',
+    price: 69.99,
+    description: 'Experience survival horror like never before in Resident Evil village. Elevate each desperate fight to survive by showcasing the most realistic and terrifying graphics to date. The all-new title is the eighth major installment in the storied Resident Evil franchise, which established the survival horror genre nearly 25 years ago. The title is currently in development using capcom\'s proprietary Re engine, used to create vivid gameplay experiences in hit titles such as Resident Evil 7 biohazard, Resident Evil 2, Resident Evil 3, and Devil May Cry 5. With hyper-detailed graphics, intense first-person action and masterful storytelling, the terror has never felt more realistic and inescapable. Set a few years after the horrifying events in the critically acclaimed Resident Evil 7 biohazard, the all-new storyline begins with Ethan winters and his wife mia living peacefully in a new location, free from their past nightmares. Just as they are building their new life together, tragedy befalls them once again. Chris Redfield, the legendary hero from the Resident Evil series, is reacquainted with the couple and horribly disrupts their life, spiraling Ethan into chaos. A devastated Ethan finds himself in a remote snow-capped village seeking answers after being thrown into an entirely new nightmare.',
+    rating: '3.5/5',
+    source: 'https://m.media-amazon.com/images/I/81TRXA773pL._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Resident-Evil-Village-Deluxe-PlayStation-5/dp/B08TR5R8S4/ref=sr_1_32?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105198&sr=8-32'
+  },
+  {
+    id: 10,
+    name: 'Sid Meier\'s Civilization VI - Nintendo Switch',
+    price: 19.89,
+    description: 'The ASTRO A40 TR Headset + Mix Amp Pro TR for PS4, PC and Mac is the premier audio solution for esports athletes, content creators and streamers. It meets esports athletes’ rigorous standards for audio fidelity, comfort and durability. The Mix Amp Pro TR features Dolby Digital Surround Sound processing and enables lag and interference-free delivery of game sound and voice communication. The A40 TR arrives as an open-back headset featuring a swappable precision mic and customizable Speaker Tags. For loud tournament environments, the headset transforms into a closed-back, noise-isolating headset by adding a Mod Kit (sold separately) with sealed Speaker Tags, synthetic leather ear cushions and a voice-isolating microphone.',
+    rating: '4.4/5',
+    source: 'https://m.media-amazon.com/images/I/81JeqskUs2L._AC_UY327_FMwebp_QL65_.jpg',
+    link: 'https://www.amazon.com/Sid-Meiers-Civilization-VI-Nintendo-Switch/dp/B07HH8ZSMM/ref=sr_1_31?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=P9RDJ1TVGHVVDXQSMT9Y&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614105198&sr=8-31'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
