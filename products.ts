c653e6cdfcb02c68b73593a203e9b055f4bc0fba
aibike28 <64321776+aibike28@users.noreply.github.com>
2021-03-03 13:34:16 +0600

    Add files via upload

---
+++ products.ts
@@ -0,0 +1,225 @@
+export const products = [
+  { category_id: 0,
+    category_name: "Electronic devices",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black',
+      price: 93.80,
+      description: 'The Acer SB220Q 21.5" Widescreen IPS display combines stylish ultra-thin functionality with amazing 1920 x 1080 resolution, allowing you to enjoy High-Definition entertainment and gaming in the comfort of your home. AMD Radeon FreeSync and rapid 4ms response time highlight the advanced technology.',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=sr_1_2?crid=TCDCS6ONQD5B&dchild=1&keywords=monitor&qid=1613997406&sprefix=mon%2Caps%2C328&sr=8-2',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg',
+      likes: 23
+    },
+    {
+      id: 1,
+      name: 'OneOdio Wired Over Ear Headphones Studio Monitor & Mixing DJ Stereo Headsets with 50mm Neodymium Drivers and 1/4 to 3.5mm Audio Jack for AMP Computer Recording Phone Piano Guitar Laptop - Black',
+      price: 30.99,
+      description: 'BASS SOUND: Enjoy clear sound and comfort with the OneOdio Studio monitor headphones. Large, 50 millimeter speaker unit drivers combined with neodymium magnets; powerful bass, clear vocal, and crisp high tones form stereo sound. It is ideal for DJ digital mixer, mixer panel, turntable, monitoring in recording studios, radio, film production, video, electronic news gathering--and virtually any application where high quality sound is required.',
+      rating: 4.5,
+      link: 'https://www.amazon.com/OneOdio-Adapter-Free-Headphones-Professional-Telescopic/dp/B01N6ZJH96/ref=sr_1_1_sspa?dchild=1&keywords=headphones&qid=1614005830&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzM1AyRzRCRjk0Q1VZJmVuY3J5cHRlZElkPUEwOTgwNDM5M01GVFpHTU9RMVYxMSZlbmNyeXB0ZWRBZElkPUEwNTE3ODYwMk8wQkU3M0QxRkxFSCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81xdLjyV5%2BL._AC_SY355_.jpg',
+      likes: 56
+    },
+    {
+      id: 2,
+      name: 'Smart Watch, Popglory Smartwatch with Blood Pressure, Blood Oxygen Monitor, Fitness Tracker with Heart Rate Monitor, Full Touch Fitness Watch for Android & iOS for Men Women',
+      price: 32.99,
+      description: 'Monitor Blood Pressure and Blood Oxygen: Adopting advanced sensors, the watch can detect your blood pressure and blood oxygen while record the data in its companion app HeroBandⅢ( Please scan the QR code in the manual to download the app), which help you know body changes better. NOTE the data cannot be used as a medical-grade test.',
+      rating: 4,
+      link: 'https://www.amazon.com/Popglory-Smartwatch-Pressure-Monitor-Fitness/dp/B08DXS7VCK/ref=sr_1_1_sspa?crid=1NDS4GJ8UAALT&dchild=1&keywords=apple+watch&qid=1614006308&sprefix=apple%2Caps%2C328&sr=8-1-spons&psc=1&smid=A3QEK68Z4UXCRO&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFORk5RV0c1SEREMUEmZW5jcnlwdGVkSWQ9QTA4NDAxNzMxU09WSzg4Qk1CUlRSJmVuY3J5cHRlZEFkSWQ9QTA4NjAyMDMyVVlVM01PV0YySURLJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71XdYJvmqBL._AC_SX679_.jpg', 
+      likes: 102
+    },
+    {
+      id: 3,
+      name: 'Dell Inspiron 17 17.3" i3793-7275SLV-PUS 10th Gen Intel Core i7-1065G7 16GB RAM 2TB HDD + 256GB SSD DVD-RW 2GB NVIDIA MX230 (1920 x 1080) Display Windows 10 Home',
+      price: 984.49,
+      description: 'Processor & Memory: 10th Gen Intel Core i7-1065G7 Processor 1.3GHz 16GB 2666MHz DDR4 RAM Drives: 2.0TB SATA 5400 RPM Hard Drive + 256GB PCIe NVMe M.2 Solid State Drive DVD-RW (Writes to DVD/CD) Operating System: Microsoft Windows 10 Home (64-bit) Communications: 802.11 Wireless-AC Qualcomm DW1810 LAN + Bluetooth 4.1 Integrated Widescreen HD 720p Webcam with Dual Digital Microphone Array 10/100 Ethernet LAN Audio Stereo Speakers with MaxxAudio Pro Graphics & Video: 17.3" Anti-Glare LED-Backlit FHD (1920 x 1080) Display 2GB NVIDIA GeForce MX230 Graphics Keyboard: Standard Keyboard Ports & Slots: 2x USB 3.1 Gen 1 1x USB 3.1 Gen 1 Type-C with Data Transition Only 1x USB 2.0 1x RJ-45 1x HDMI Out 1.4b 1x Combination Headphone/Microphone Jack 1x Media Card Reader Power Supply: 3-Cell 42WHr Battery Additional Information: Dimensions: 16.35" x 10.99" x 0.98" Approximate Weight: 6.13 lbs',
+      rating: 4.3,
+      link: 'https://www.amazon.com/Dell-Inspiron-i3793-7275SLV-PUS-i7-1065G7-Display/dp/B081K9QZDF/ref=sr_1_8?dchild=1&keywords=laptop&qid=1614006639&sr=8-8',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51cUNtjCjVL._AC_SX522_.jpg',
+      likes: 5
+    },
+    {
+      id: 4,
+      name: 'SAMSUNG QN32Q50RAFXZA Flat 32" QLED 4K 32Q50 Series Smart TV',
+      price: 447.99,
+      description: 'Buy, rent and watch the newest movies, plus subscribe to and watch all new Apple TV channels like HBO, Showtime and Starz on the new Apple TV app. Plus with AirPlay 2-enabled Samsung Smart TVs, customers can also effortlessly play videos from their Apple device directly to their Samsung Smart TV.',
+      rating: 4.7,
+      link: 'https://www.amazon.com/SAMSUNG-QN32Q50RAFXZA-32Q50-Smart-TV2019/dp/B07W5QYD2K/ref=sr_1_1?dchild=1&keywords=tv+65+inch+smart+tv+4k&qid=1614007487&s=electronics&sr=1-1',
+      image: 'https://m.media-amazon.com/images/S/aplus-media/vc/1b2b5fe9-1f15-4e52-af21-89d4ab192c8b.__CR0,0,1000,750_PT0_SX800_V1___.jpg',
+      likes: 165
+    }]
+  },
+  { category_id: 1,
+    category_name: "For house",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Veryhome Fake Succulent Plants Artificial Faux Succulents 4pcs Plastic Mini Potted Fake Succulents for Flower Arrangements',
+      price: 11.29,
+      description: 'REALISTIC and REAL TOUCH】These artificial succulents has life-like looking , vivid color,real touch material.Mini fake succulents are the best choice for those who shares no time for live succulents.These faux succulents potted are really very cute,also Safer than real ones for houses with pets. Stay close to nature without watering the floral plants.',
+      rating: 4.3,
+      link: 'https://www.amazon.com/Veryhome-Fake-Succulent-Plants-Artificial-Faux-Succulents-4pcs-Plastic-Mini-Potted-Fake-Succulents-Flower-Arrangements/dp/B07DXP3KMG/ref=sr_1_2?dchild=1&keywords=plants&qid=1614006896&sr=8-2',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61tgGHypqML._AC_SL1500_.jpg', 
+      likes: 1004
+    },
+    {
+      id: 1,
+      name: 'Vonanda Sofa Bed, Convertible Chair 4 in 1 Multi-Function Folding Ottoman Modern Breathable Linen Guest Bed with Adjustable Sleeper for Small Room Apartment,Light Brown',
+      price: 399.99,
+      description: 'CONVERTIBLE DESIGN:The convertible sofa bed can be used as a ottoman in normal. It can be easily and quickly converted into a sofa, lounger or a bed to meet your various and versatile needs and no longer worry about unexpected guests.Perfect choice for small room,dorms, apartments, studios,office.',
+      rating: 4.5,
+      link: 'https://www.amazon.com/Vonanda-Convertible-Multi-Function-Breathable-Adjustable/dp/B08BJ5V27J/ref=sr_1_9?dchild=1&keywords=sofa&qid=1614670284&s=kitchen-intl-ship&sr=1-9',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71NLprEQC5L._AC_SL1200_.jpg',
+      likes: 2456
+    },
+    {
+      id: 2,
+      name: 'Zinus 6 Inch Green Tea Memory Foam Mattress / CertiPUR-US Certified / Bed-in-a-Box / Pressure Relieving, Narrow Twin',
+      price: 118.11,
+      description: 'THE RIGHT COMBINATION - Our special recipe for rejuvenating rest? Refreshing green tea and moisture-absorbing Activ charcoal infused into pressure-relieving memory foam that cradles your shape so you awake free of aches and pains and fresh as a daisy',
+      rating: 4.5,
+      link: 'https://www.amazon.com/Zinus-Memory-Replacement-Mattress-Narrow/dp/B074QTSBS2/ref=sr_1_12?dchild=1&keywords=bed&qid=1614670407&s=kitchen-intl-ship&sr=1-12',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/91MCpnM34gL._AC_SY450_.jpg',
+      likes: 356
+    },
+    {
+      id: 3,
+      name: 'Modern Soft Indoor Fluffy Shag Fur Area Rug for Bedroom Livingroom Dorm Kids Room Home Decorative Floor Carpet, Non-Slip Comfy Plush Furry Fur Accent Rugs 4x6 Feet, Grey',
+      price: 31.99,
+      description: 'This Shag Fur Rug Made of the Softest Premium Synthetic Microfiber, the Fur Touched Silky Smooth Felling, Create a Cozy Area and Keep Your Feet Away from the Icy Floor in the Cold Winter. This modern area rug is perfect for bedroom and those medium traffic areas in your home. Featurs ultra soft, fuzzy, fluffy plush, does not contain any harmful substances, no smell, no shed. This high quality decorative rugs is also kids and pets friendly',
+      rating: 4.5,
+      link: 'https://www.amazon.com/Shaggy-Bedroom-Nursery-Decorative-Livingroom/dp/B081F454LL/ref=sr_1_2?dchild=1&keywords=carpet&qid=1614670491&s=kitchen-intl-ship&sr=1-2',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81Lo%2B0M8JSL._AC_SY450_.jpg',
+      likes: 734
+    },
+    {
+      id: 4,
+      name: 'JOISCOPE Home Office Computer Desk,Small Study Writing Desk with Wooden Storage Shelf,2-Tier Industrial Morden Laptop Table with Splice Board,47 inches(Vintage Oak Finish)',
+      price: 86.00,
+      description: 'MULTIFUNCTIONAL DESIGN：Our table has a multi-purpose storage function, providing two-layer shelf, which can save space for you, and the middle board can be disassembled to place larger objects, such as your computer host. EASY ASSEMBLY：With the easily comprehensible instructions, no great effort is required to set up this table.Our installation can be done by a single person.In addition, we also provide you with a detailed installation video, you can refer to the manual and video to complete the installation.',
+      rating: 4.6,
+      link: 'https://www.amazon.com/JOISCOPE-Computer-Shelves-Laptop-Vintage/dp/B083PVDB91/ref=sr_1_18?dchild=1&keywords=table&qid=1614670573&s=kitchen-intl-ship&sr=1-18',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81%2Ba-b51zWL._AC_SL1500_.jpg',
+      likes: 465
+    }]
+  },
+  { category_id: 2,
+    category_name: "Sport",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'YOSUDA Indoor Cycling Bike Stationary - Cycle Bike with Ipad Mount ＆Comfortable Seat Cushion (Gray)',
+      price: 336.99,
+      description: '35lbs flywheel and heavy-duty steel frame of the exercise bike guarantee the stability while cycling. The belt driven system provides a smoother and quieter ride than chain transport. It wont disturb your apartment neighbors or sleeping kids.',
+      rating: 4.4,
+      link: 'https://www.amazon.com/YOSUDA-Indoor-Cycling-Bike-Stationary/dp/B07D528W98/ref=sr_1_2?dchild=1&keywords=bike&qid=1614667893&s=sporting-goods&sr=1-2',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61zKUmCY0DL._AC_UX385_.jpg',
+      likes: 506
+    },
+    {
+      id: 1,
+      name: '10-In-1 Ab Wheel Roller Kit with Resistance Bands, Knee Mat, Jump Rope, Push-Up Bar - Home Gym Equipment for Men Women',
+      price: 29.97,
+      description: 'Super Comprehensive Exercise Set- 11 Pcs Ab Wheel Roller Kit : 1* AB Roller Wheel, 1* Thick Foam Knee Mat , 2* Resistance bands, 2* Push Up Bars Handles Grips, 2* Wrist Wraps, 1* 8-shaped Stretched Band, 1* Jump Rope, 1* Guide Book, 1* Gift Box Package. 11-IN-1 ultra-wide ab roller wheel is designed to help you build stronger, bigger abdominal muscles. Improve your overall health and exercise at home, gym or office.',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Roller-Wheel-Resistance-Bands-Push-Up/dp/B087J9ZB98/ref=sr_1_4?dchild=1&keywords=gym&qid=1614668232&s=sporting-goods&sr=1-4',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71X0HU5mbnL._AC_SL1500_.jpg',
+      likes: 432
+    },
+    {
+      id: 2,
+      name: 'Under Armour Adult Undeniable Duffle 4.0 Gym Bag',
+      price: 76.99,
+      description: 'UA Storm technology delivers an element-battling, highly water-resistant finish that keeps you dry in any wet weather situation. Large vented and zippered pockets for organization. Large vented pocket for laundry or shoes, internal slip pockets for organization, side mesh pocket',
+      rating: 4.8,
+      link: 'https://www.amazon.com/Under-Armour-Undeniable-Duffel-4-0/dp/B07J37SYBW/ref=sr_1_20?dchild=1&keywords=gym&qid=1614668232&s=sporting-goods&sr=1-20',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71P6jCSls5L._AC_UX385_.jpg',
+      likes: 709
+    },
+    {
+      id: 3,
+      name: 'Neleus Mens Dry Fit Performance Athletic Shirt with Hoods',
+      price: 35.89,
+      description: 'Neleus was born in 2008 ,with its unique design concept, excellent quality and comfortable clipping, has gain the crazy love from the clients. As the most energetic brand, Neleus is committed to provide the most innovative and price-friendly sport goods. Technological quality is the priority of the companys products. ',
+      rating: 4.5,
+      link: 'https://www.amazon.com/Neleus-Running-Shirt-Workout-Athletic/dp/B07WVRDZTT/ref=sr_1_4?crid=6M8M4ZBJFRIQ&dchild=1&keywords=gym+shirts&qid=1614668542&sprefix=gym+shi%2Caps%2C334&sr=8-4',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71a2roWfYhL._AC_UY445_.jpg',
+      likes: 1045
+    },
+    {
+      id: 4,
+      name: 'Spalding NBA Street Outdoor Basketball',
+      price: 17.99,
+      description: 'Performance: Durable outdoor rubber cover. Superior control: Deep channel design for easy handles. Official size: Size 7, 29.5. Inflation: Shipped inflated and we recommend 8 PSI',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Spalding-NBA-Street-Basketball-Official/dp/B0009VELG4/ref=sr_1_3?crid=1LS0MT7MR17YK&dchild=1&keywords=basketball+ball&qid=1614668649&sprefix=bas%2Caps%2C338&sr=8-3',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/91PlxEj64%2BL._AC_SL1500_.jpg',
+      likes: 698
+    }]
+  },
+  { category_id: 3,
+    category_name: "Automobile",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Mpow Car Phone Mount, Dashboard Windshield Car Phone Holder with Long Arm, Strong Sticky Gel Suction Cup, Anti-Shake Stabilizer Compatible iPhone 12 11 pro/11 pro max/XS/XR/X/8/7,Galaxy, Moto and More',
+      price: 15.99,
+      description: 'FREE INSTALLATION & UNOBSTRUCTED SIGHT: Mpow focuses on the innovative and smarter mounting solution and blazes past other traditional dashboard car mounts. Up to 15cm/5.9in aluminum long gooseneck sends the dashboard Phone mount much closer to you without blocking your sight, giving you the freedom to position it on multiple locations on the dashboard or windshield. Most importantly, it can be bent to avoid obstructing display and irregular vents',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Mpow-Windshield-Anti-Shake-Stabilizer-Compatible/dp/B07X2WGT72/ref=sr_1_9?dchild=1&qid=1614670862&s=automotive-intl-ship&sr=1-9',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61gc90tHQcL._AC_SX425_.jpg',
+      likes: 238
+    },
+    {
+      id: 1,
+      name: 'TICARVE Cleaning Gel for Car Detailing Tools Keyboard Cleaner Automotive Dust Air Vent Interior Detail Detailing Putty Universal Dust Cleaner for Auto Laptop Home Car Slime Cleaner',
+      price: 7.49,
+      description: 'Dust cleaning gel for car is perfect for cleaning the nooks and crannies in your car or truck. Cleaning car gel cleaner is super great to get into the vents and little crevices on the car air vents, steering wheels, console panel, storage bins, cup holder, door handles, dashboards to pick up the dirt',
+      rating: 4.1,
+      link: 'https://www.amazon.com/TICARVE-Cleaning-Detailing-Automotive-Universal/dp/B07VCBQ6KF/ref=sr_1_7?dchild=1&qid=1614670862&s=automotive-intl-ship&sr=1-7',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71j60x8bWxL._AC_SY355_.jpg',
+      likes: 932
+    },
+    {
+      id: 2,
+      name: 'LISEN Car Phone Holder Mount, [Upgraded Clip] Magnetic Phone Car Mount [6 Strong Magnets] Cell Phone Holder for Car [Case Friendly] iPhone Car Holder Compatible with 4-6.7 inch Smartphones & Tablets',
+      price: 12.99,
+      description: 'This Car Phone Holder Mount is designed as “hook shape” and form “Three grasp points” which can vise the outlet blades tightly and ensure it never fall off in any situation. The Cell Phone Holder for Car iS BORNED FOR INNOVATION & PRACTICABILITY, it solving the market’s common problem of being easy to fall off over time.',
+      rating: 4.4,
+      link: 'https://www.amazon.com/LISEN-Magnetic-Upgraded-Compatible-Smartphone/dp/B07YRKDF4P/ref=sr_1_48?dchild=1&qid=1614671149&s=automotive-intl-ship&sr=1-48',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71anUhTBjvL._AC_SX425_.jpg',
+      likes: 1035
+    },
+    {
+      id: 3,
+      name: 'SEALIGHT 9005/HB3 H8/H11 LED Headlight Bulbs, 14000LM High Low Beam, Combo Package CSP Led Chips Hi/Lo lights - 6000K Xenon White',
+      price: 69.99,
+      description: 'Increase Your Confidence While Driving at Night: The 9005 H11 LED headlight bulbs are 7000lm per bulb, 6000K optics xenon white, allowing you to see further and clearer for safer driving, also perfect for a pickup truck. 10 Minutes Easy Installation: Plug-and-play, Canbus Ready, 1:1 mini design 0.5-inch ultra-thin heat sink, the SEALIGHT led headlights perfectly fit for 99% cars even with a dust cover factory housing',
+      rating: 4.6,
+      link: 'https://www.amazon.com/SEALIGHT-9005-HB3-H11-Chips-12000LM/dp/B07JNY3ZVZ/ref=sr_1_84?dchild=1&qid=1614671243&s=automotive-intl-ship&sr=1-84',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/714DF8pGjmL._AC_SL1500_.jpg',
+      likes: 447
+    },
+    {
+      id: 4,
+      name: 'Dash Cam Front and Rear CHORTAU Dual Dash Cam 3 inch Dashboard Camera Full HD 170° Wide Angle Backup Camera with Night Vision WDR G-Sensor Parking Monitor Loop Recording Motion Detection',
+      price: 49.98,
+      description: 'This dual dash cam equipped with 1080P resolution front camera and waterproof rear camera.The car dashcam with 6 pcs IR LED and Wide Dynamic Range, the front camera captures the license plates and road signs clearly even at night, thus ensuring to get you the best footage. Wide-angle front (170°) and rear（130°）camera help eliminate the blind spots of your vision. ',
+      rating: 4.3,
+      link: 'https://www.amazon.com/CHORTAU-Dashboard-G-Sensor-Recording-Detection/dp/B07Q5R22DV/ref=sr_1_86?dchild=1&qid=1614671434&s=automotive-intl-ship&sr=1-86',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71C4tTMoBnL._AC_SY355_.jpg',
+      likes: 150
+    }]
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
